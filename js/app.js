359709e534cf78b3d47d743838f4767a140e1017
Erol <eroltutumlu@gmail.com>
2020-12-19 21:33:53 +0300

    feat: add expense

--- js/app.js
+++ js/app.js
@@ -32,6 +32,57 @@ class UI {
     }
   }
 
+  submitExpenseForm(){
+    const expenseValue = this.expenseInput.value;
+    const amountValue = this.amountInput.value;
+    if(expenseValue === '' || amountValue === '' || amountValue < 0){
+      this.expenseFeedback.classList.add('showItem');
+      this.expenseFeedback.innerHTML = `<p>Value can not be empty or negative</p>`;
+      const self = this;
+      setTimeout(function(){
+        self.expenseFeedback.classList.remove('showItem');
+      }, 4000)
+    }else{
+      let amount = parseInt(amountValue);
+      this.expenseInput.value = "";
+      this.amountInput.value = "";
+
+      let expense = {
+        id: this.itemID,
+        title: expenseValue,
+        amount: amount,
+      }
+
+      this.itemID++;
+      this.itemList.push(expense);
+      this.addExpense(expense);
+      this.showBalance();
+    }
+  }
+
+  addExpense(expense){
+    const div = document.createElement('div');
+    div.classList.add('expense');
+    div.innerHTML = `
+     
+        <div class="expense-item d-flex justify-content-between align-items-baseline">
+         <h6 class="expense-title mb-0 text-uppercase list-item">- ${expense.title}</h6>
+         <h5 class="expense-amount mb-0 list-item">${expense.amount}</h5>
+         <div class="expense-icons list-item">
+            <a href="#" class="edit-icon mx-2" data-id="${expense.id}">
+              <i class="fas fa-edit"></i>
+            </a>
+            <a href="#" class="delete-icon" data-id="${expense.id}">
+              <i class="fas fa-trash"></i>
+            </a>
+          </div>
+        </div>
+  
+    `;
+
+    this.expenseList.appendChild(div);
+  }
+
   showBalance(){
     const expense = this.totalExpense();
     const total = parseInt(this.budgetAmount.textContent) - expense;
@@ -49,7 +100,16 @@ class UI {
   }
 
   totalExpense(){
-    let total = 300;
+    let total = 0;
+
+    if(this.itemList != null && this.itemList.length > 0){
+      total = this.itemList.reduce((acc, curr) => {
+        acc += curr.amount;
+        return acc;
+      }, 0);
+    }
+
+    this.expenseAmount.textContent = total;
     return total;
   }
 
@@ -71,13 +131,11 @@ function eventListeners() {
 
   expenseForm.addEventListener('submit', function(e){
     e.preventDefault();
-
-
+    ui.submitExpenseForm();
   })
 
   expenseList.addEventListener('click', function(e){
     e.preventDefault();
-
   })
 
 }
